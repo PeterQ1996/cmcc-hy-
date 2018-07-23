2804c74e7597d7c5c5a4ba9eaea9827f724715d1
Peer-To-Peer <1366276261@qq.com>
2018-07-23 15:28:11 +0800

    python3.6->python3.5 修改ContentGet_360

--- com-fj-phishing-2/com/fj/src/mysql_save_result/MySqlSave.py
+++ com-fj-phishing-2/com/fj/src/mysql_save_result/MySqlSave.py
@@ -8,53 +8,56 @@ import time
 import datetime
 # sys.path.extend(['/home/qianhuhai/Documents/PycharmProjects/com-fj-phishing-2'])
 # print(os.getcwd())
-projectPath=os.getcwd()
-# 打开数据库连接
-db = pymysql.connect(host="172.28.19.238", user="AthenaR",
-                     password="GYzKyTeB", db="tele_fraud", port=33061, charset='utf8')
-cur = db.cursor()
-f=open(projectPath+"/web_scan_probe/web-content",'r')
-for i in f.readlines():
-    hostname, ip, ipBelong, firstVisitTime, lastestVisitTime, userSet, visitNum, similarityValue, imitate,md5_filename,level\
-        =i.strip('\r\n').split(',')
+def ConnectToSql():
+    os.chdir('/home/qianhuhai/PycharmProjects/com-fj-phishing-2/')
+    projectPath=os.getcwd()
+    # 打开数据库连接
+    db = pymysql.connect(host="172.28.19.238", user="AthenaR",
+                         password="GYzKyTeB", db="test_qhh", port=33061, charset='utf8')
+    cur = db.cursor()
 
-    userSet=userSet.strip(' ').replace("'",'')
-    visitNum = visitNum.strip(' ').replace("'", '')
-    hostname = hostname.strip(' ').replace("'", '')
-    ip = ip.strip(' ').replace("'", '')
-    imitate=imitate.strip(' ').replace("'", '')
-    md5_filename = md5_filename.strip(' ').replace("'", '')
-    lastestVisitTime=lastestVisitTime.strip(' ').replace("'", '')
+    f=open(projectPath+"/web_scan_probe/web-content",'r')
+    for i in f.readlines():
+        hostname, ip, ipBelong, firstVisitTime, lastestVisitTime, userSet, visitNum, similarityValue, imitate,md5_filename,level\
+            =i.strip('\r\n').split(',')
 
-    strUserSet=int(userSet)
-    strVisitNum=int(visitNum)
-    strHostName=hostname
-    strIp=ip
-    strImitate=imitate
-    strMd5_Filename= 'png/'+md5_filename
-    lastestVisitTime=int(lastestVisitTime)
-    lastestVisitTime=int(lastestVisitTime/1000)
+        userSet=userSet.strip(' ').replace("'",'')
+        visitNum = visitNum.strip(' ').replace("'", '')
+        hostname = hostname.strip(' ').replace("'", '')
+        ip = ip.strip(' ').replace("'", '')
+        imitate=imitate.strip(' ').replace("'", '')
+        md5_filename = md5_filename.strip(' ').replace("'", '')
+        lastestVisitTime=lastestVisitTime.strip(' ').replace("'", '')
 
-    found_time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        strUserSet=int(userSet)
+        strVisitNum=int(visitNum)
+        strHostName=hostname
+        strIp=ip
+        strImitate=imitate
+        strMd5_Filename= 'png/'+md5_filename
+        lastestVisitTime=int(lastestVisitTime)
+        lastestVisitTime=int(lastestVisitTime/1000)
 
-    time_local = time.localtime(lastestVisitTime)
+        found_time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    lastTime = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+        time_local = time.localtime(lastestVisitTime)
 
-    ipBelong=ipBelong.strip(' ').replace("'",'').encode('utf-8').decode('utf-8')
+        lastTime = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
 
-    # strIpBelong=ipBelong.encode('GB2312').decode('utf-8')
-    # print(strIpBelong)
+        ipBelong=ipBelong.strip(' ').replace("'",'').encode('utf-8').decode('utf-8')
 
-    cur.execute("""insert into t_fraud_web_phish(id,domain,ip,ip_belongs,found_time,last_time,user_num,count,imitate,evidence_link)
-         values(null,'%s', '%s', '%s', '%s', '%s', '%d', '%d', '%s', '%s')"""
-                % ( strHostName, strIp,ipBelong ,found_time ,
-                    lastTime, strUserSet, strVisitNum,  strImitate, strMd5_Filename))
+        # strIpBelong=ipBelong.encode('GB2312').decode('utf-8')
+        # print(strIpBelong)
 
-    # cur.execute("""insert into t_mcws_web_phishing(id,domain,ip,ip_belongs,found_time,last_time,count,user_count,imitate,evidence_link,handle)
-    #  values(NULL ,'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s')"""
-    #             % (hostname, ip, ipBelong, firstVisitTime, lastestVisitTime, userSet, visitNum, similarityValue, imitate,md5_filename))
+        cur.execute("""insert into t_fraud_web_phish(id,domain,ip,ip_belongs,found_time,last_time,user_num,count,imitate,evidence_link,handle)
+             values(null,'%s', '%s', '%s', '%s', '%s', '%d', '%d', '%s', '%s' ,'%s')"""
+                    % ( strHostName, strIp,ipBelong ,found_time ,
+                        lastTime, strUserSet, strVisitNum,  strImitate, strMd5_Filename ,'0' ))
 
-    db.commit()
-cur.close()
-db.close()
+        # cur.execute("""insert into t_mcws_web_phishing(id,domain,ip,ip_belongs,found_time,last_time,count,user_count,imitate,evidence_link,handle)
+        #  values(NULL ,'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s')"""
+        #             % (hostname, ip, ipBelong, firstVisitTime, lastestVisitTime, userSet, visitNum, similarityValue, imitate,md5_filename))
+
+        db.commit()
+    cur.close()
+    db.close()
